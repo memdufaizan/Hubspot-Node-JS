62e6e28c7389a648a1df3cd566b7bb4f9da014f8
Faizan Memdu <45518852+memdufaizan@users.noreply.github.com>
2023-10-11 11:35:47 +0530

    Add, Update, Delete contact via HubSpot API
    Hubspot add, update, delete contact via HubSpot API

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ app.get('/', async (req, res) => {
 
     try {
         const response = await axios.get(contacts, { headers });
-        const data = response.data.results;
+        const data = response.data.results.slice(2); //removed first two sample contact
         // const res_demo = await contactModule.demo();
-        // console.log('RES DEMO - >>>' + res_demo);
+        //console.log('RES DEMO - >>>' + data.properties);
         res.render('contacts', { title: 'All Contact from Hubspot', data });
     } catch (error) {
         console.error(error);
@@ -50,7 +50,7 @@ app.post('/add-contact', async (req,res) => {
     if (email == '' || firstname == '' || lastname == '' || phone == '' || company == '') {
         const resdata = {
             status: false,
-            message: 'Required fields are missing'
+            message: 'Required fields are missing - Email, Firstname, Lastname, Phone, Company'
         }
         res.render('add-contact', {title: 'Add new contact', resdata: resdata});
     } else {
@@ -80,6 +80,120 @@ app.post('/add-contact', async (req,res) => {
         console.log('Module Res->>>' + contact.status);
         res.render('add-contact', {title: 'Add new contact', resdata: resdata});
     }
+})
+
+app.get('/delete/:id', (req, res) => {
+    //console.log(req.params.id);
+	const ContactIdToDelete = req.params.id;
+	
+	const delete_contacts_url = API_BASE_URL + `/crm/v3/objects/contacts/${ContactIdToDelete}`;
+    const headers = {
+        'Authorization': `Bearer ${PRIVATE_APP_TOKEN}`,
+        'Content-Type': 'application/json'
+    }
+
+	axios.delete(delete_contacts_url, { headers })
+	.then(response => {
+		console.log(`Deleted contact with ID ${ContactIdToDelete}`);
+	})
+	.catch(error => {
+		console.error(error);
+	});
+    res.redirect('back');
+})
+
+app.get('/edit-contact/:id', async (req, res) => {
+	//console.log(req.params.id);
+	const ContactIdToEdit = req.params.id;
+	if(ContactIdToEdit != '') {
+		const contact = await contactModule.getContact(ContactIdToEdit);
+		if (contact) {
+			const data = {
+				id: req.params.id,
+				email: contact.properties.email,
+				firstname: contact.properties.firstname,
+				lastname: contact.properties.lastname,
+				phone: contact.properties.phone,
+				company: contact.properties.company,
+				website: contact.properties.website,
+				address: contact.properties.address,
+				city: contact.properties.city,
+				state: contact.properties.state,
+				country: contact.properties.country,
+				zip: contact.properties.zip,
+				jobtitle: contact.properties.jobtitle
+			}
+
+			res.render('edit-contact', { title: 'Edit contact', data: data, resdata: ''});
+		} else {
+			console.log('Contact not found.');
+		}
+		
+	} else {
+		
+	}
+    
+})
+
+app.post('/edit-contact/:id', async (req, res) => {
+    const ContactIdToEdit = req.params.id;
+	if(ContactIdToEdit != '') {
+		const { email, firstname, lastname, phone, company, website, address, city, state, country, zip, jobtitle } = req.body;
+
+        if (email == '' || firstname == '' || lastname == '' || phone == '' || company == '') {
+            const resdata = {
+                status: false,
+                message: 'Required fields are missing - Email, Firstname, Lastname, Phone, Company'
+            }
+            const data = {
+                id: req.params.id,
+                email: email,
+                firstname: firstname,
+                lastname: lastname,
+                phone: phone,
+                company: company,
+                website: website,
+                address: address,
+                city: city,
+                state: state,
+                country: country,
+                zip: zip,
+                jobtitle: jobtitle,
+                submit: false
+            }
+            res.render('edit-contact', { title: 'Edit contact', data: data, resdata: resdata});
+        } else {
+            const data = {
+                id: req.params.id,
+                email: email,
+                firstname: firstname,
+                lastname: lastname,
+                phone: phone,
+                company: company,
+                website: website,
+                address: address,
+                city: city,
+                state: state,
+                country: country,
+                zip: zip,
+                jobtitle: jobtitle,
+                submit: true
+            }
+            
+            const contact = await contactModule.editContact(data);
+            
+            const resdata = {
+                status: contact.status,
+                message: contact.msg
+            }
+
+            //console.log('Module Res->>>' + contact.status);
+            res.render('edit-contact', {title: 'Edit contact', data: data, resdata: resdata});
+        }
+		
+	} else {
+		console.log('Edit ID not found.');
+	}
 })
 
 app.listen(3000, () => console.log('App running on http://localhost:3000'));
--- module/contact.js
+++ module/contact.js
@@ -5,11 +5,32 @@ exports.demo = async function () {
     return 'this is from demo';
 };
 
+const API_BASE_URL = process.env.API_BASE_URL;
+const PRIVATE_APP_TOKEN = process.env.PRIVATE_APP_ACCESS_TOKEN;
+
+exports.getContact = async function (id) {
+    const getUrl = API_BASE_URL + `/crm/v3/objects/contacts/${id}?properties=company&properties=email&properties=firstname&properties=lastname&properties=phone&properties=website&properties=address&properties=city&properties=state&properties=country&properties=zip&properties=jobtitle`;
+    const headers = {
+        'Authorization': `Bearer ${PRIVATE_APP_TOKEN}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        const response = await axios.get(getUrl, { headers });
+        const data = response.data.results;
+        //console.log('RES GET - >>>' + util.inspect(response.data, {depth: null}));
+        return response.data;
+        //res.render('contacts', { title: 'All Contact from Hubspot', data });
+    } catch (error) {
+        console.error(error);
+    }
+
+    
+};
 
 exports.addContact = async function (postdata) {
     //console.log(postdata);
-    const postUrl = process.env.API_BASE_URL + '/crm/v3/objects/contacts';
-    const PRIVATE_APP_TOKEN = process.env.PRIVATE_APP_ACCESS_TOKEN;
+    const postUrl = API_BASE_URL + '/crm/v3/objects/contacts';
     
     const headers = {
         'Authorization': `Bearer ${PRIVATE_APP_TOKEN}`,
@@ -49,5 +70,41 @@ exports.addContact = async function (postdata) {
     } catch (error) {
         // If there's an error, catch it and return an error object.
         return { status: false, msg: 'Error while create contact' };
+    }
+};
+
+exports.editContact = async function (postdata) {
+    //console.log(postdata);
+    const editUrl = API_BASE_URL + `/crm/v3/objects/contacts/${postdata.id}`;
+    
+    const headers = {
+        'Authorization': `Bearer ${PRIVATE_APP_TOKEN}`,
+        'Content-Type': 'application/json'
+    };
+
+    const properties = {
+        "email": postdata.email,
+        "firstname": postdata.firstname,
+        "lastname": postdata.lastname,
+        "phone": postdata.phone,
+        "company": postdata.company,
+        "website": postdata.website,
+        "address": postdata.address,
+        "city": postdata.city,
+        "state": postdata.state,
+        "country": postdata.country,
+        "zip": postdata.zip,
+        "jobtitle": postdata.jobtitle,
+        "hubspot_owner_id": "561241889",
+    };
+    
+    try {
+        const response = await axios.patch(editUrl, { properties }, { headers: headers });
+        //console.log('AXIOS PATCH ->>>' + util.inspect(response.data, {depth: null}));
+        // If the request is successful, return the data in the response.
+        return { status: true, msg: 'Contact edited successfully' };
+    } catch (error) {
+        // If there's an error, catch it and return an error object.
+        return { status: false, msg: 'Error while edit contact' };
     }
 };
